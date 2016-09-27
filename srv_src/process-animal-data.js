758cd96a05b6df50235a6c76befea479dd6a354e
Szab Virag <szab.virag@gmail.com>
2016-09-27 16:45:05 +1000

    Server request animal data and transforms it for easy UI display.

--- srv_src/process-animal-data.js
+++ srv_src/process-animal-data.js
@@ -1,8 +1,40 @@
 import requestPromise from 'request-promise';
 
-export default function(url){
-	return requestPromise(url).then((data) => {
-		console.log(data);
-		return data;
-	}); 
+// Transform the base data so that the UI can easily display it. 
+// Function returns a JSON string with cat names by gender of owner.
+export function animalDataTransformer (data){
+	const baseData = JSON.parse(data);
+
+	// Transform the array to an object that lists cat names by gender of owner
+	const results = baseData.reduce((results, person) => {
+
+		// Get cat names for this person, some people don't have pets
+		const catNames = person.pets 
+			? person.pets.filter( (animal) => animal.type === "Cat" )
+				.map((animal) => animal.name) 
+			: [];
+
+		// Update add the cat names to the relevant array by the users gender.
+		const namesArray = results[person.gender];
+		namesArray.push.apply(namesArray, catNames);
+
+		return results;
+	}, { Male: [], Female: [] } );
+
+	// Sort the cat names in alphabetically.
+	return JSON.stringify({ 
+		Male: results.Male.sort(), 
+		Female: results.Female.sort(),
+	});
+}
+
+// Function requests the animal data form the web service and transforms it 
+// to be displayed in the UI.
+export default function(uri){
+	var options = {
+		uri,
+		transform: animalDataTransformer
+	};
+
+	return requestPromise(options);
 }
